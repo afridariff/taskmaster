b55dc7c3521bfcd84f564719d9798a3462eea586
aafrid <aafrid@ceiamerica.com>
2024-03-21 15:54:11 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AllTasksComponent } from './pages/all-tasks/all-tasks.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { MenuComponent } from './shared/menu/menu.component';
+
+// Define your routes
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'Home', component: HomeComponent },
+  { path: 'all-tasks', component: AllTasksComponent },
+  { path: '**', component: ErrorComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AllTasksComponent,
+    ErrorComponent,
+    MenuComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
